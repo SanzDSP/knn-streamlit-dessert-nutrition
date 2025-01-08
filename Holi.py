164cfff83d772606f6a393a62a42ba11b3efcac7
SanzDSP <137406888+SanzDSP@users.noreply.github.com>
2025-01-08 11:58:11 +0700

    Update Holi.py

--- Holi.py
+++ Holi.py
@@ -7,15 +7,6 @@ from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsClassifier
 import io
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-import h5py
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
-from sklearn.neighbors import KNeighborsClassifier
-import io
-
 # Fungsi untuk memuat data klasterisasi dari file .h5
 def load_clusters(file_path):
     with h5py.File(file_path, 'r') as h5_file:
